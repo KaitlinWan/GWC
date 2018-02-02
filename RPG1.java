8d4db19273ffb9201997bb840ad4c9f16b57a0ab
Kaitlin <kaitlin.wan@gmail.com>
2018-02-02 12:55:13 -0500

    this

---
+++ RPG1.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+public Class RPG1{
+  //Adding User input
+  public void playGame(){
+    //1 System.out.println("WOW!");
+    //3 Scanner <name> = <name>.
+  }
+
+  public static void main(String[] args){
+    System.out.println("Hello! Welcome to my RPG!");
+    //2 playGame();
+  }
+}
